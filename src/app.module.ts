9a4af9b2a01de7dffb6aeeac8a46af6f9c0b6df1
Eunbin-Son <thsdmsqlsspdlqj@naver.com>
2021-07-17 15:39:53 +0900

    commit changes

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://eunbin:eunbin@cluster0.1lb6u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
